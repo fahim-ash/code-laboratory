1a768d7606f67add4e385d43d2e9de3074edc274
fahim-ash <ashhabfahim@gmail.com>
2025-01-10 10:33:16 +0600

    demo model and serilalizer updated

--- back_end/api_gateway/app/models.py
+++ back_end/api_gateway/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Demo(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+
+    @property
+    def name_with_date(self):
+        return str(self.name) + str(self.date)
