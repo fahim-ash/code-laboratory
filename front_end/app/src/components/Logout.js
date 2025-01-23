51a74a1efad062014ce50768475e17451734824f
Milon <milon153769@gmail.com>
2025-01-23 23:45:36 +0600

    logout api url changed.

--- front_end/app/src/components/Logout.js
+++ front_end/app/src/components/Logout.js
@@ -8,7 +8,7 @@ const Logout = () => {
     useEffect(() => {
         const handleLogout = async () => {
             try {
-                await axios.post("http://localhost:8000/api/logout/", {}, { withCredentials: true });
+                await axios.post("/api/logout/", {}, { withCredentials: true });
                 navigate("/");
             } catch (error) {
                 console.error("Logout failed:", error);
